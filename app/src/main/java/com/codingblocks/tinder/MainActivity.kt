2dd8419f983fe685b85607566b864f0b8f9393cb
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-06-15 03:04:34 +0530

    create LoginActivity

--- app/src/main/java/com/codingblocks/tinder/MainActivity.kt
+++ app/src/main/java/com/codingblocks/tinder/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.codingblocks.tinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.mobileBtn
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mobileBtn.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
     }
 }
