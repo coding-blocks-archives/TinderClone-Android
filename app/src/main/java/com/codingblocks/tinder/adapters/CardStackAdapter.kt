8fa2e0598aaa8c1529b89bba92ca9754a04d08b3
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-08-19 18:25:01 +0530

    Swipe Images on Click

--- app/src/main/java/com/codingblocks/tinder/adapters/CardStackAdapter.kt
+++ app/src/main/java/com/codingblocks/tinder/adapters/CardStackAdapter.kt
@@ -35,6 +35,7 @@ class CardStackAdapter(
     fun getUsers(): List<User> = users
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var currentImage = 0
         val indicators = arrayOfNulls<ImageView?>(6)
         fun bind(user: User) {
             with(user) {
@@ -57,6 +58,32 @@ class CardStackAdapter(
                     } else {
                         Picasso.get().load(s).fetch()
 
+                    }
+                }
+
+            }
+
+            itemView.left_click.setOnClickListener {
+                if (currentImage > 0) {
+                    currentImage = --currentImage
+                    Picasso.get().load(user.photos[currentImage]).into(itemView.item_image)
+                    indicators[currentImage]?.setImageDrawable(itemView.resources.getDrawable(R.drawable.selecteditem))
+                    indicators.forEachIndexed { index, view ->
+                        if (index != currentImage)
+                            view?.setImageDrawable(itemView.resources.getDrawable(R.drawable.nonselecteditem))
+                    }
+                } else {
+
+                }
+            }
+            itemView.right_click.setOnClickListener {
+                if (currentImage < user.photos.size - 1) {
+                    currentImage = ++currentImage
+                    Picasso.get().load(user.photos[currentImage]).into(itemView.item_image)
+                    indicators[currentImage]?.setImageDrawable(itemView.resources.getDrawable(R.drawable.selecteditem))
+                    indicators.forEachIndexed { index, view ->
+                        if (index != currentImage)
+                            view?.setImageDrawable(itemView.resources.getDrawable(R.drawable.nonselecteditem))
                     }
                 }
             }
